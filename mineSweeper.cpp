c33f21757aca869ebaf33897ad37b8d93f788356
kstillwell1 <stillwell.kevin@icloud.com>
2026-07-08 13:57:12 -0400

    Create mineSweeper.cpp

---
+++ mineSweeper.cpp
@@ -0,0 +1,338 @@
+#include "tile.h"
+#include "mineSweeper.h"
+#include <iostream>
+#include <utility>
+#include <queue>
+#include <thread>
+#include <chrono>
+
+MineSweeperGame::~MineSweeperGame()
+{
+	if (board.board != nullptr)
+	{
+		for (int i = 0; i < getNumRows(); i++)
+		{
+			delete[] board.board[i];
+
+		}
+		delete[] board.board;
+	}
+}
+
+void MineSweeperGame::setNumRows(int rows)
+{
+	numRows = rows;
+}
+
+void MineSweeperGame::setNumCols(int cols)
+{
+	numCols = cols;
+}
+
+int MineSweeperGame::getNumRows()
+{
+	return numRows;
+}
+
+int MineSweeperGame::getNumCols()
+{
+	return numCols;
+}
+
+void MineSweeperGame::setNumMines(int mineCount)
+{
+	numMines = mineCount;
+}
+
+int MineSweeperGame::getNumMines()
+{
+	return numMines;
+}
+
+void MineSweeperGame::setMines(int mines)
+{
+	srand(time(NULL));
+	int currSetMines = 0;
+	int row = 0, col = 0;
+	int maxRow = getNumRows();
+	int maxCol = getNumCols();
+
+	while (currSetMines != mines)
+	{
+		row = std::rand() % maxRow;
+		col = std::rand() % maxCol;
+
+		if (board.board[row][col].mine == false)
+		{
+			board.board[row][col].mine = true;
+			board.board[row][col].actionedTile = true;
+			currSetMines++;
+		}
+	}
+}
+
+void MineSweeperGame::allocateBoard(int row, int col, int mines)
+{
+	if (board.board != nullptr)
+	{
+		for (int i = 0; i < row; i++)
+		{
+			delete[] board.board[i];
+
+		}
+		delete[] board.board;
+	}
+
+	board.board = new Tile * [row];
+
+	for (int i = 0; i < row; i++)
+	{
+		board.board[i] = new Tile[col];
+	}
+
+	setMines(mines);
+}
+
+void MineSweeperGame::userDifficultySelection()
+{
+	int answer;
+	std::cout << "Welcome to Mine Sweeper, Please enter the number of the difficulty you would like to play: " << std::endl;
+	std::cout << "1. EASY\n";
+	std::cout << "2. MEDIUM\n";
+	std::cout << "3. HARD\n";
+
+	std::cin >> answer;
+
+	switch (answer)
+	{
+		case (1):
+		{
+			setDifficulty(9, 9, 10); //easy difficulty
+			break;
+		}
+		case (2):
+		{
+			setDifficulty(16, 16, 40); //medium difficulty
+			break;
+		}
+		case (3):
+		{
+			setDifficulty(30, 16, 99); //hard difficulty
+			break;
+		}
+		default:
+		{
+			std::cout << "Invalid Selection";
+			userDifficultySelection();
+		}
+	}
+}
+
+void MineSweeperGame::setDifficulty(int rows, int cols, int mines)
+{
+	setNumRows(rows);
+	setNumCols(cols);
+	setNumMines(mines);
+	allocateBoard(numRows, numCols, numMines);
+}
+
+void MineSweeperGame::printBoard()
+{
+	double timeToReveal = 5.0 / numMines;
+	std::cout << "  | ";
+	for (int col = 0; col < numCols; ++col)
+	{
+		if (col < 10)
+		{
+			std::cout << col << " | ";
+		}
+		else
+		{
+			std::cout << col << "| ";
+		}
+	}
+	std::cout << "\n";
+
+
+	for (int row = 0; row < numRows; ++row)
+	{
+		if (row < 10)
+		{
+			std::cout << row << " | ";
+		}
+		else
+		{
+			std::cout << row << "| ";
+		}
+		for (int col = 0; col < numCols; ++col)
+		{
+			if (board.board[row][col].hitMine == true)
+			{
+				std::cout << "M";
+				std::this_thread::sleep_for(std::chrono::duration<double>(timeToReveal));
+			}
+			else if (gameEnd == true && board.board[row][col].mine == true)
+			{
+				std::cout << "F";
+			}
+			else if (board.board[row][col].revealed == true)
+			{
+				if (checkAdjacentTiles(row, col) == 0)
+				{
+					std::cout << " ";
+				}
+				else
+				{
+					std::cout << checkAdjacentTiles(row, col);
+				}
+			}
+			else
+			{
+				std::cout << "-";
+			}
+			std::cout << " | ";
+		}
+		std::cout << "\n";
+	}
+}
+
+void MineSweeperGame::revealTiles(int row, int col)
+{
+	std::queue<std::pair<int, int>> tilesToReveal;
+	tilesToReveal.push({ row, col });
+
+	while (!tilesToReveal.empty())
+	{
+		std::pair<int, int> current = tilesToReveal.front();
+		tilesToReveal.pop();
+
+		int r = current.first, c = current.second;
+
+		if (r < 0 || r >= numRows || c < 0 || c >= numCols) { continue; }
+		if (board.board[r][c].revealed == true) { continue; }
+
+		int currAdjacentMines = checkAdjacentTiles(r, c);
+
+		if (currAdjacentMines == 0)
+		{
+			tilesToReveal.push({ r - 1, c - 1 });
+			tilesToReveal.push({ r - 1, c });
+			tilesToReveal.push({ r - 1, c + 1 });
+			tilesToReveal.push({ r, c - 1 });
+			tilesToReveal.push({ r, c + 1 });
+			tilesToReveal.push({ r + 1, c - 1 });
+			tilesToReveal.push({ r + 1, c });
+			tilesToReveal.push({ r + 1, c + 1 });
+		}
+	}
+
+	//recursion
+	/*if (row < 0 || row >= numRows || col < 0 || col >= numCols) { return; }
+	if (board.board[row][col].revealed == true) { return; }
+
+	int adjacentMines = checkAdjacentTiles(row, col);
+
+	if (adjacentMines == 0)
+	{
+		revealTiles(row - 1, col - 1);
+		revealTiles(row - 1, col);
+		revealTiles(row - 1, col + 1);
+		revealTiles(row, col - 1);
+		revealTiles(row, col + 1);
+		revealTiles(row + 1, col - 1);
+		revealTiles(row + 1, col);
+		revealTiles(row + 1, col + 1);
+	}*/
+}
+
+int MineSweeperGame::checkAdjacentTiles(int row, int col)
+{
+	int adjacentMineNum = 0;
+	if (row < 0 || row >= numRows || col < 0 || col >= numCols) { return adjacentMineNum; }
+	{
+		board.board[row][col].revealed = true;
+		board.board[row][col].actionedTile = true;
+
+		if (row - 1 >= 0 && col - 1 >= 0 && board.board[row - 1][col - 1].mine == true) { adjacentMineNum++; }
+		if (row - 1 >= 0 && board.board[row - 1][col].mine == true) { adjacentMineNum++; }
+		if (row - 1 >= 0 && col + 1 < numCols && board.board[row - 1][col + 1].mine == true) { adjacentMineNum++; }
+		if (col - 1 >= 0 && board.board[row][col - 1].mine == true) { adjacentMineNum++; }
+		if (col + 1 < numCols && board.board[row][col + 1].mine == true) { adjacentMineNum++; }
+		if (row + 1 < numRows && col - 1 >= 0 && board.board[row + 1][col - 1].mine == true) { adjacentMineNum++; }
+		if (row + 1 < numRows && board.board[row + 1][col].mine == true) { adjacentMineNum++; }
+		if (row + 1 < numRows && col + 1 < numCols && board.board[row + 1][col + 1].mine == true) { adjacentMineNum++; }
+	}
+	return adjacentMineNum;
+}
+
+void MineSweeperGame::userTurn()
+{
+	int row, col;
+	std::cout << "Enter row: ";
+	std::cin >> row;
+	std::cout << "Enter column: ";
+	std::cin >> col;
+
+	if (row < 0 || row >= numRows || col < 0 || col >= numCols || board.board[row][col].revealed == true)
+	{
+		std::cout << "Invalid input, please try again " << std::endl;
+		userTurn();
+	}
+	else if (board.board[row][col].mine == true)
+	{
+		timedReveal();
+		gameEnd = true;
+		std::cout << "You hit a mine " << std::endl;
+	}
+	else
+	{
+		revealTiles(row, col);
+	}
+}
+
+bool MineSweeperGame::checkWin()
+{
+	for (int row = 0; row < numRows; row++)
+	{
+		for (int col = 0; col < numCols; col++)
+		{
+			if (board.board[row][col].actionedTile == false) return false;
+		}
+	}
+	return true;
+}
+
+void MineSweeperGame::timedReveal()
+{
+	for (int row = 0; row < numRows; row++)
+	{
+		for (int col = 0; col < numCols; col++)
+		{
+			if (board.board[row][col].mine == true)
+			{
+				board.board[row][col].hitMine = true;
+			}
+		}
+	}
+}
+
+void MineSweeperGame::gameLoop()
+{
+	userDifficultySelection();
+	while (gameEnd != true)
+	{
+		printBoard();
+		userTurn();
+		if (gameEnd == true)
+		{
+			printBoard();
+			break;
+		}
+		if (checkWin())
+		{
+			gameEnd = true;
+			printBoard();
+			std::cout << "Congrats on your win" << std::endl;
+		}
+	}
+}
