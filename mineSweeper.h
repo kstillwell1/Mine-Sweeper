f03270459894ade7948917599ea4bea41f86c991
kstillwell1 <stillwell.kevin@icloud.com>
2026-07-08 13:55:58 -0400

    Create mineSweeper.h

---
+++ mineSweeper.h
@@ -0,0 +1,34 @@
+#pragma once
+#include "tile.h"
+
+class MineSweeperGame
+{
+private:
+	Board board;
+	bool gameEnd = false;
+	int numRows = 0;
+	int numCols = 0;
+	int numMines = 0;
+
+	void allocateBoard(int row, int col, int mines);
+	void setMines(int mines);
+	void userDifficultySelection();
+	void printBoard();
+	int checkAdjacentTiles(int row, int col);
+	void revealTiles(int row, int col);
+	void userTurn();
+	bool checkWin();
+	void timedReveal();
+	void setDifficulty(int rows, int cols, int mines);
+public:
+
+	~MineSweeperGame();
+	void setNumRows(int rows);
+	int getNumRows();
+	void setNumCols(int Cols);
+	int getNumCols();
+	void setNumMines(int mineCount);
+	int getNumMines();
+
+	void gameLoop();
+};
