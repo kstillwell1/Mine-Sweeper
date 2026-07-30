c948044355df40accfc2ad0e688850d3cb90ac52
kstillwell1 <stillwell.kevin@icloud.com>
2026-07-30 17:05:36 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,17 @@
 #include "mineSweeper.h"
+#include "windows.h"
+#include <iostream>
 
 int main()
 {
+	std::ios::sync_with_stdio(false);
+
+	HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+	DWORD dwMode = 0;
+	GetConsoleMode(hOut, &dwMode);
+	dwMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+	SetConsoleMode(hOut, dwMode);
+
 	MineSweeperGame game;
 	game.gameLoop();
 
